ef5166440c265688ecb4c1f84c38cd79ace282c1
brooksrog8 <114731707+brooksrog8@users.noreply.github.com>
2024-01-13 11:42:00 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -148,7 +148,7 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
 });
